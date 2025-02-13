cb510529a9b8c8dd2e81e4437fbb48fe3d94b883
andreagarbugli <andrea.garbugli@unibo.it>
2025-02-13 15:23:08 +0000

    Add dependency installation script and update build scripts. Start working on the XDP perf application.

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 CC=gcc
-CFLAGS="-Wall -Wextra -Werror -std=c11"
+CFLAGS="-std=c11"
+CFLAGS="$CFLAGS -Wall -Wextra -Werror"
 LDFLAGS="-lm -ldl -lpthread"
 DEFINES=""
 
-BUILD_TYPE=debug
+BUILD_TYPE=0 		# 0: Debug, 1: Release
+BUILD_TYPE_STR=debug
 PROJECT_NAME=all
 
 if [ $# -eq 1 ]; then
@@ -13,21 +15,25 @@ elif [ $# -eq 2 ]; then
     PROJECT_NAME=$2
 fi
 
-if [ $BUILD_TYPE = "debug" ]; then
+if [ $BUILD_TYPE = "debug" ] || [ $BUILD_TYPE = "0" ]; then
     CFLAGS="$CFLAGS -g"
     DEFINES="$DEFINES -DDEBUG -DNSN_ENABLE_LOGGER"
-elif [ $BUILD_TYPE = "release" ]; then
+elif [ $BUILD_TYPE = "release" ] || [ $BUILD_TYPE = "1" ]; then
     # Maybe remove the NSN_ENABLE_LOGGER define in release builds
     DEFINES="$DEFINES -DNSN_ENABLE_LOGGER"
-    CFLAGS="$CFLAGS -O3"
+    CFLAGS="$CFLAGS -O3 -march=native -mavx2"
+    BUILD_TYPE_STR=release
 else
     echo "Invalid build type: $BUILD_TYPE"
     exit 1
 fi
 
+echo "Compiling in '${BUILD_TYPE_STR}' mode"
 cd build
-$CC $CFLAGS $LDFLAGS ../src/nsnd.c $DEFINES -o nsnd 
-$CC $CFLAGS $LDFLAGS ../src/nsn_app_tx.c $DEFINES -o nsn-app-tx
-$CC $CFLAGS $LDFLAGS ../src/nsn_app_rx.c $DEFINES -o nsn-app-rx
-$CC $CFLAGS $LDFLAGS ../src/nsn_app_perf.c $DEFINES -o nsn-perf
+set -x
+$CC $CFLAGS ../src/nsnd.c 	  $LDFLAGS $DEFINES -o nsnd
+$CC $CFLAGS ../src/nsn_app_tx.c   $LDFLAGS $DEFINES -o nsn-app-tx
+$CC $CFLAGS ../src/nsn_app_rx.c   $LDFLAGS $DEFINES -o nsn-app-rx
+$CC $CFLAGS ../src/nsn_app_perf.c $LDFLAGS $DEFINES -o nsn-perf
+set +x
 cd ..
