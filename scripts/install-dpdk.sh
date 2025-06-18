fbd7c7145f149502ccbc36385888107a9c09695b
ellerre <lorenzo.rosa.bo@gmail.com>
2025-06-18 07:28:51 +0000

    apply patch for dpdk in the install script

--- scripts/install-dpdk.sh
+++ scripts/install-dpdk.sh
@@ -24,6 +24,7 @@ sudo apt install -y librdmacm-dev libmnl-dev build-essential clang libnuma-dev p
 set -e
 
 # Switch to working directory.
+home=$(pwd)
 pushd $PWD
 mkdir -p $HOME/tmp/dpdk
 cd $HOME/tmp/dpdk
@@ -32,6 +33,7 @@ cd $HOME/tmp/dpdk
 git clone https://github.com/DPDK/dpdk.git
 cd dpdk
 git checkout v22.11
+git apply "$home/../dpdk_22_11_mods.diff"
 
 # Build and install
 meson setup --buildtype=release --prefix=$INSTALL_DIR build
